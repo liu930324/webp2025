4100a8bd734a90a38e77d242ad9a72fdbe9d7da4
liu930324 <113400161+liu930324@users.noreply.github.com>
2025-05-20 15:53:55 +0800

    Delete hw4 directory

--- hw4/src/utils/api.js
+++
@@ -1,7 +0,0 @@
-export const fetchEventData = async () => {
-  const response = await fetch(
-    "https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=6"
-  );
-  const data = await response.json();
-  return data;
-};
